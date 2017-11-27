af01bace66f01ec5b5f3f755f6cdacb1b03b9713
azaremoth <azaremoth@users.noreply.github.com>
2017-11-27 21:36:37 +0100

    cleaned up spawn script

--- LuaRules/Gadgets/game_spawn.lua
+++ LuaRules/Gadgets/game_spawn.lua
@@ -32,20 +32,29 @@ if (not gadgetHandler:IsSyncedCode()) then
 	return false
 end
 
-
 local Gaia = Spring.GetGaiaTeamID
 local modOptions = Spring.GetModOptions()
 local cheatAItype = "0"
-local startbase = "1"
+local cpvmode = false
+local cpvstartbase = false
 local luaSetStartPositions = {}
 	
 local ChickenAIs = VFS.Include("LuaRules/Configs/ai_chickenlist.lua")	
 
-local cvmode = false
+-- Spring.Echo("CPV mode " .. Spring.GetModOptions().scoremode)
+-- Spring.Echo("CPV mode " .. Spring.GetModOptions().startbase)
 
 if (Spring.GetModOptions().scoremode ~= nil) then
 	if (Spring.GetModOptions().scoremode ~= "disabled") then
-		cvmode = true
+		cpvmode = true
+	end
+end
+
+if (cpvmode and Spring.GetModOptions().startbase ~= nil) then
+	if (Spring.GetModOptions().startbase == "1") then
+		cpvstartbase = true
+	else
+		cpvstartbase = false
 	end
 end
 
@@ -108,7 +117,73 @@ local function SetStartLocation(teamID, x, z)
 end
 GG.SetStartLocation = SetStartLocation
 
-local function GetStartUnit(teamID)
+---- Chicken Spawns -------------------------------------------
+local function SpawnChicken (teamID, x, y, z)
+	local zero = Spring.CreateUnit("tc_cursedhand_survival", x-50,y,z+50, math.random(3), teamID)
+	local one = Spring.CreateUnit("tc_cursedhand_survival", x+100,y,z-100, math.random(3), teamID)	
+end
+
+---- Cursed Spawns -------------------------------------------
+local function SpawnBaseCursed (teamID, x, y, z)
+	local zero = Spring.CreateUnit("tc_heart_ai", x,y,z, math.random(3), teamID)
+	local one = Spring.CreateUnit("tc_defender", x+160,y,z+200, math.random(3), teamID)
+	local two = Spring.CreateUnit("tc_defender", x-180,y,z-170, math.random(3), teamID)		
+	local three= Spring.CreateUnit("tc_defender", x+180,y,z-210, math.random(3), teamID)
+	local four= Spring.CreateUnit("tc_defender", x-200,y,z+160, math.random(3), teamID)
+	local six= Spring.CreateUnit("tc_defender", x-225,y,z-20, math.random(3), teamID)
+	local seven= Spring.CreateUnit("tc_defender", x+230,y,z, math.random(3), teamID)
+end
+
+local function SpawnUnitsCursed (teamID, x, y, z)
+	local ghoul = Spring.CreateUnit("tc_ghoul", x-30, y, z-70, math.random(3), teamID)
+	local gunner = Spring.CreateUnit("tc_gunner", x+110, y, z-50, math.random(3), teamID)
+	local witch = Spring.CreateUnit("tc_witch", x, y, z+70, math.random(3), teamID)	
+	local hero = Spring.CreateUnit("tc_shade_lvl1", x-80, y, z, math.random(3), teamID)	
+end
+
+local function SpawnBuildersCursed (teamID, x, y, z)
+	local builder1 = Spring.CreateUnit("tc_obelisk", x+90,y,z, math.random(3), teamID)
+	local builder2 = Spring.CreateUnit("tc_necromancer", x+60,y,z+60, math.random(3), teamID)
+end
+
+local function SpawnAIBuildersCursed (teamID, x, y, z)
+	local builder1 = Spring.CreateUnit("tc_restless", x+150,y,z, math.random(3), teamID)			
+	local builder2 = Spring.CreateUnit("tc_restless", x-150,y,z, math.random(3), teamID)
+end
+
+---- Imperial Spawns -------------------------------------------
+local function SpawnBaseImperials (teamID, x, y, z)
+	local zero = Spring.CreateUnit("euf_techcenter_shield_ai", x,y,z,  math.random(3), teamID)
+	local one = Spring.CreateUnit("euf_plasmatower", x+250,y,z+250,  math.random(3), teamID)
+	local two = Spring.CreateUnit("euf_plasmatower", x-250,y,z-250,  math.random(3), teamID)
+	local three= Spring.CreateUnit("euf_plasmatower", x+250,y,z-250,  math.random(3), teamID)
+	local four= Spring.CreateUnit("euf_plasmatower", x-250,y,z+250,  math.random(3), teamID)
+	local five= Spring.CreateUnit("euf_aatower", x-200,y,z,  math.random(3), teamID)
+	local six= Spring.CreateUnit("euf_aatower", x+200,y,z,  math.random(3), teamID)
+end
+
+local function SpawnUnitsImperials (teamID, x, y, z)
+	local marine = Spring.CreateUnit("euf_marine", x-130, y, z, 0, teamID)
+	local marine2 = Spring.CreateUnit("euf_marine", x-100, y, z, 0, teamID)
+	local sniper = Spring.CreateUnit("euf_sniper", x-130, y, z-30, 0, teamID)
+	local priest = Spring.CreateUnit("euf_priest", x-100, y, z-30, 0, teamID)
+	local hero = Spring.CreateUnit("euf_sarge_lvl1", x-115, y, z+60, 2, teamID)
+end
+
+local function SpawnBuildersImperials (teamID, x, y, z)
+	local builder1 = Spring.CreateUnit("euf_constructor", x+80, y, z-90, math.random(3), teamID)
+	local builder2 = Spring.CreateUnit("euf_constructor", x, y, z+90, math.random(3), teamID)
+end
+
+local function SpawnAIBuildersImperials (teamID, x, y, z)
+	local builder1 = Spring.CreateUnit("euf_constructor_ai", x, y, z-130, math.random(3), teamID)
+	local builder2 = Spring.CreateUnit("euf_constructor_ai", x, y, z+130, math.random(3), teamID)
+end
+
+----------------------------------------------------------------
+
+
+local function SpawnStartUnit(teamID)
 	-- get the team startup info
 	local side = select(5, Spring.GetTeamInfo(teamID))
 	local ai = select(4, Spring.GetTeamInfo(teamID))
@@ -124,11 +199,6 @@ local function GetStartUnit(teamID)
 	if Spring.GetModOptions().cheatingai ~= nil then
 		cheatAItype = Spring.GetModOptions().cheatingai
 	end 
-	if (cvmode and Spring.GetModOptions().startbase ~= nil) then
-		startbase = Spring.GetModOptions().startbase
-	else
-		startbase = "0"
-	end
 	
 	if (side == "" or side == nil) then -- startscript didn't specify a side for this team, ramdomly pick one
 		if (math.random() > 0.5) then
@@ -140,132 +210,34 @@ local function GetStartUnit(teamID)
 	GG.teamside = side
 	
 	if (IsChickenAI) then
-		local heading = math.random(3)
-		local zero = Spring.CreateUnit("tc_cursedhand_survival", x-50,y,z+50, heading, teamID)
-		local heading = math.random(3)		
-		local one = Spring.CreateUnit("tc_cursedhand_survival", x+100,y,z-100, heading, teamID)					
+		SpawnChicken (teamID, x, y, z)				
 	else
-		if (side == "imperials") then	
-			local heading = math.random(3)
-			local sniper = Spring.CreateUnit("euf_sniper", x-80, y, z+80, heading, teamID)
-			local heading = math.random(3)			
-			local priest = Spring.CreateUnit("euf_priest", x, y, z-100, heading, teamID)
-			local heading = math.random(3)
-			local marine = Spring.CreateUnit("euf_marine", x, y, z+100, heading, teamID)
-			local heading = math.random(3)
-			local marine2 = Spring.CreateUnit("euf_marine", x+80, y, z+80, heading, teamID)			
-			local heading = math.random(3)
-			local hero = Spring.CreateUnit("euf_sarge_lvl1", x-100, y, z-70, heading, teamID)
-			if (startbase == "1") then
-					local heading = math.random(3)			
-					local zero = Spring.CreateUnit("euf_techcenter_shield_ai", x,y,z, heading, teamID)
-					local one = Spring.CreateUnit("euf_plasmatower", x+250,y,z+250, heading, teamID)
-					local two = Spring.CreateUnit("euf_plasmatower", x-250,y,z-250, heading, teamID)
-					local three= Spring.CreateUnit("euf_plasmatower", x+250,y,z-250, heading, teamID)
-					local four= Spring.CreateUnit("euf_plasmatower", x-250,y,z+250, heading, teamID)
-					local five= Spring.CreateUnit("euf_aatower", x-200,y,z, heading, teamID)
-					local six= Spring.CreateUnit("euf_aatower", x+200,y,z, heading, teamID)
-					if ((ai == true) and (team ~= Gaia)) then
-						local builder1 = Spring.CreateUnit("euf_constructor_ai", x, y, z-150, heading, teamID)
-						local builder2 = Spring.CreateUnit("euf_constructor_ai", x, y, z+150, heading, teamID)
-					else
-						local builder1 = Spring.CreateUnit("euf_constructor", x, y, z-150, heading, teamID)
-						local builder2 = Spring.CreateUnit("euf_constructor", x, y, z+150, heading, teamID)
-					end	
-			elseif ((ai == true) and (team ~= Gaia)) then
-				local heading = math.random(3)
-				local builder1 = Spring.CreateUnit("euf_constructor_ai", x, y, z, heading, teamID)			
-				if cheatAItype == "1" then
-					local zero = Spring.CreateUnit("euf_techcenter_shield_ai", x,y,z, heading, teamID)
-					local one = Spring.CreateUnit("euf_plasmatower", x+250,y,z+250, heading, teamID)
-					local two = Spring.CreateUnit("euf_plasmatower", x-250,y,z-250, heading, teamID)
-					local three= Spring.CreateUnit("euf_plasmatower", x+250,y,z-250, heading, teamID)
-					local four= Spring.CreateUnit("euf_plasmatower", x-250,y,z+250, heading, teamID)
-					local five= Spring.CreateUnit("euf_aatower", x-200,y,z, heading, teamID)
-					local six= Spring.CreateUnit("euf_aatower", x+200,y,z, heading, teamID)
-					local seven= Spring.CreateUnit("euf_constructor_ai", x,y,z-150, 1, teamID)
-				end
-
-			else
-				local heading = math.random(3)
-				local builder1 = Spring.CreateUnit("euf_constructor", x, y, z, heading, teamID)
+		if (side == "imperials") then
+			SpawnUnitsImperials (teamID, x, y, z)
+			if ((cpvstartbase == true) or ((ai == true) and (team ~= Gaia) and (cheatAItype == "1"))) then
+				SpawnBaseImperials (teamID, x, y, z)
 			end
+			if ((ai == true) and (team ~= Gaia)) then
+				SpawnAIBuildersImperials (teamID, x, y, z)
+			else
+				SpawnBuildersImperials (teamID, x, y, z)
+			end	
+	
 		else
-			local heading = math.random(3)
-			local hero = Spring.CreateUnit("tc_shade_lvl1", x-60, y, z-60, heading, teamID)		
-			if (startbase == "1") then
-				local heading = math.random(3)
-				local zero = Spring.CreateUnit("tc_heart_ai", x,y,z, heading, teamID)
-				local one = Spring.CreateUnit("tc_defender", x+180,y,z+200, heading, teamID)
-				local two = Spring.CreateUnit("tc_defender", x-200,y,z-170, heading, teamID)
-				local heading = math.random(3)				
-				local three= Spring.CreateUnit("tc_defender", x+200,y,z-210, heading, teamID)
-				local four= Spring.CreateUnit("tc_defender", x-220,y,z+160, heading, teamID)
-				local six= Spring.CreateUnit("tc_bonebeast", x-75,y,z, 4, teamID)
-				local seven= Spring.CreateUnit("tc_gunner", x,y,z+75, 1, teamID)
-				local nine= Spring.CreateUnit("tc_ghoul", x,y,z+100, 1, teamID)
-				if ((ai == true) and (team ~= Gaia)) then
-					local builder1 = Spring.CreateUnit("tc_restless", x+220,y,z, 1, teamID)			
-					local builder2 = Spring.CreateUnit("tc_restless", x-220,y,z, 1, teamID)
-				else
-					local builder1 = Spring.CreateUnit("tc_necromancer", x+220,y,z, 1, teamID)			
-					local builder2 = Spring.CreateUnit("tc_necromancer", x-220,y,z, 1, teamID)
-				end	
-			elseif ((ai == true) and (team ~= Gaia)) then			
-				if cheatAItype == "1" then
-					local heading = math.random(3)		
-					local zero = Spring.CreateUnit("tc_heart_ai", x,y,z, heading, teamID)
-					local one = Spring.CreateUnit("tc_defender", x+180,y,z+200, heading, teamID)
-					local two = Spring.CreateUnit("tc_defender", x-200,y,z-170, heading, teamID)
-					local three= Spring.CreateUnit("tc_defender", x+200,y,z-210, heading, teamID)
-					local four= Spring.CreateUnit("tc_defender", x-220,y,z+160, heading, teamID)
-					local five= Spring.CreateUnit("tc_restless", x+150,y,z, heading, teamID)
-					local six= Spring.CreateUnit("tc_enforcer", x-75,y,z, 4, teamID)
-					local seven= Spring.CreateUnit("tc_gunner", x,y,z+75, 1, teamID)
-					local eight= Spring.CreateUnit("tc_restless", x,y,z-125, 1, teamID)
-					local nine= Spring.CreateUnit("tc_skeleton", x,y,z+100, 1, teamID)
-					local ten= Spring.CreateUnit("tc_witch", x+220,y,z, 1, teamID)
-					local eleven= Spring.CreateUnit("tc_witch", x-220,y,z, 1, teamID)
-				else
-					local heading = math.random(3)				
-					local one= Spring.CreateUnit("tc_restless", x-50,y,z, heading, teamID)
-					local two= Spring.CreateUnit("tc_mage", x+50,y,z, heading, teamID)
-					local three= Spring.CreateUnit("tc_gunner", x,y,z-50, heading, teamID)
-					local four= Spring.CreateUnit("tc_ghoul", x,y,z+50, heading, teamID)
-				end	
-			else
-				local heading = math.random(3)
-				local obelisk = Spring.CreateUnit("tc_obelisk", x, y, z, heading, teamID)
-				local heading = math.random(3)
-				local ghoul = Spring.CreateUnit("tc_ghoul", x-60, y, z+40, heading, teamID)
-				local heading = math.random(3)
-				local gunner = Spring.CreateUnit("tc_gunner", x, y, z+60, heading, teamID)
-				local heading = math.random(3)
-				local necromancer = Spring.CreateUnit("tc_necromancer", x+40, y, z+60, heading, teamID)
-				local heading = math.random(3)				
-				local witch = Spring.CreateUnit("tc_witch", x, y, z-50, heading, teamID)				
+			SpawnUnitsCursed (teamID, x, y, z)
+			if ((cpvstartbase == true) or ((ai == true) and (team ~= Gaia) and (cheatAItype == "1"))) then
+				SpawnBaseCursed (teamID, x, y, z)
 			end
+			if ((ai == true) and (team ~= Gaia)) then
+				SpawnAIBuildersCursed (teamID, x, y, z)
+			else
+				SpawnBuildersCursed (teamID, x, y, z)
+			end	
 		end
 	end
 end
 
-local function SpawnStartUnit(teamID)
-	local startUnit = GetStartUnit(teamID)
-	if (startUnit and startUnit ~= "") then
-		-- spawn the specified start unit
-		local x,y,z = Spring.GetTeamStartPosition(teamID)
-		-- snap to 16x16 grid
-		x, z = 16*math.floor((x+8)/16), 16*math.floor((z+8)/16)
-		y = Spring.GetGroundHeight(x, z)
-		-- facing toward map center
-		local facing=math.abs(Game.mapSizeX/2 - x) > math.abs(Game.mapSizeZ/2 - z)
-			and ((x>Game.mapSizeX/2) and "west" or "east")
-			or ((z>Game.mapSizeZ/2) and "north" or "south")
-		local unitID = Spring.CreateUnit(startUnit, x, y, z, facing, teamID)
-		-- set the *team's* lineage root
-		Spring.SetUnitLineage(unitID, teamID, true)
-	end
-
+local function SetStartingResources(teamID)
 	-- set start resources, either from mod options or custom team keys
 	local teamOptions = select(7, Spring.GetTeamInfo(teamID))
 	local m = modOptions.startmetal or teamOptions.startmetal or 1000
@@ -299,6 +271,7 @@ function gadget:GameStart()
 		-- don't spawn a start unit for the Gaia team
 		if (teamID ~= gaiaTeamID) then
 			SpawnStartUnit(teamID)
+			SetStartingResources(teamID)
 		end
 	end	
 end

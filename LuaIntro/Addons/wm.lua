ca00a670b85f939ae14cf3244fb0ac0eb90a9cff
azaremoth <azaremoth@users.noreply.github.com>
2018-02-08 21:47:13 +0100

    Update wm.lua

--- LuaIntro/Addons/wm.lua
+++ LuaIntro/Addons/wm.lua
@@ -17,5 +17,5 @@ function addon.Initialize()
 	local version = (Game and Game.version) or (Engine and Engine.version) or "Engine version error"
 	Spring.SetWMIcon("LuaUI/Images/ZK_logo.png")
 	--Spring.SetWMCaption(name .. " (Spring " .. version .. ")", name)
-	Spring.SetWMCaption("Zero-K", "Zero-K")
+	Spring.SetWMCaption("The Cursed", "The Cursed")
 end

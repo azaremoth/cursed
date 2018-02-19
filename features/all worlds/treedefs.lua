26de5da63a53e4f01240f0b974960fcc3d55a708
azaremoth <azaremoth@users.noreply.github.com>
2018-02-19 23:36:18 +0100

    added 3 different tree sizes

--- features/all worlds/treedefs.lua
+++ features/all worlds/treedefs.lua
@@ -1,13 +1,24 @@
 -- $Id$
 
 local objects = {
-	"features/behe/behepine_regular_2.s3o",
-	"features/behe/behepine_regular_3.s3o",
-	"features/behe/behepine_regular_1.s3o",
-
 	"features/behe/behepine_brown_1.s3o",
 	"features/behe/behepine_brown_2.s3o",
 	"features/behe/behepine_brown_3.s3o",
+	"features/behe/behepine_regular_1.s3o",
+	"features/behe/behepine_regular_2.s3o",
+	"features/behe/behepine_regular_3.s3o",
+	"features/behe/behepine_big_brown_1.s3o",
+	"features/behe/behepine_big_brown_2.s3o",
+	"features/behe/behepine_big_brown_3.s3o",
+	"features/behe/behepine_big_regular_1.s3o",
+	"features/behe/behepine_big_regular_2.s3o",
+	"features/behe/behepine_big_regular_3.s3o",	
+	"features/behe/behepine_huge_brown_1.s3o",
+	"features/behe/behepine_huge_brown_2.s3o",
+	"features/behe/behepine_huge_brown_3.s3o",
+	"features/behe/behepine_huge_regular_1.s3o",
+	"features/behe/behepine_huge_regular_2.s3o",
+	"features/behe/behepine_huge_regular_3.s3o",	
 }
 
 local treeDefs = {}
@@ -17,6 +28,7 @@ local function CreateTreeDef(i)
      blocking    = true,
      burnable    = true,
      reclaimable = true,
+	 upright 	 = true,	 
      energy      = 25,
      damage      = 5,
      metal       = 0,
@@ -25,7 +37,7 @@ local function CreateTreeDef(i)
      object = objects[(i % #objects) + 1] ,
      footprintX  = 2,
      footprintZ  = 2,
-     collisionVolumeScales = [[20 42 20]],
+     collisionVolumeScales = [[22 75 22]],
      collisionVolumeType = [[cylY]],
 
      customParams = {

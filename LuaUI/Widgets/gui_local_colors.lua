a486e7359c30279ebce35d3aaaa4920928fab377
azaremoth <azaremoth@users.noreply.github.com>
2018-03-10 23:53:03 +0100

    you will always keep your own color

--- LuaUI/Widgets/gui_local_colors.lua
+++ LuaUI/Widgets/gui_local_colors.lua
@@ -30,16 +30,34 @@ options = {
 
 local gaiaColor = {}
 local myColor = {}
+local myTrueColor = {}
 local allyColors = {}
 local enemyColors = {}
+local rand1 = (0.25*math.random())
+local rand2 = (0.25*math.random())
+local teamRand1 = {}
+local teamRand2 = {}
+for _, teamID in ipairs(Spring.GetTeamList()) do
+	teamRand1[teamID]=(0.50*math.random())
+	teamRand2[teamID]=(0.75*math.random())
+end
 
-gaiaColor[1] = 0.25
-gaiaColor[2] = 0.25
-gaiaColor[3] = 0.25
+local myTeam = Spring.GetMyTeamID()
+
+mtr,mtg,mtb,_ = Spring.GetTeamColor(myTeam)
+
+-- Use your set color in any case
+myTrueColor[1] = mtr
+myTrueColor[2] = mtg
+myTrueColor[3] = mtb
 
 myColor[1] = 0.0
 myColor[2] = 0.2
 myColor[3] = 0.8
+
+gaiaColor[1] = 0.25
+gaiaColor[2] = 0.25
+gaiaColor[3] = 0.25
 
 allyColors[1] = 0.0
 allyColors[2] = 1.0
@@ -62,41 +80,40 @@ local function SetNewTeamColors()
 	Spring.SetTeamColor(gaia, unpack(gaiaColor))
 	
 	local myAlly = Spring.GetMyAllyTeamID()
-	local myTeam = Spring.GetMyTeamID()
 	local teams = Spring.GetTeamList()
 
-	local a = 0.35
-	local e = 0.35
-	local increase = 0.35
+	local a = 0.30
+	local e = 0.30
+	local increase = 0.25
 	
 	for _, teamID in ipairs(Spring.GetTeamList()) do
 		local _,_,_,_,_,allyID = Spring.GetTeamInfo(teamID)
 		if (allyID == myAlly) then
-			local r = allyColors[1]
 			local g = allyColors[2]*a
-			local b = math.random()*g
+			local r = teamRand1[teamID]*g
+			local b = teamRand2[teamID]*g
 			Spring.SetTeamColor(teamID, r,g,b)
 			
 			a = a+increase
 			if (a > 1) then 
-				a = math.random()
+				a = 0.1 + rand1
 			end
 			
 		elseif (teamID ~= gaia) then		
 			local r = enemyColors[1]*e
-			local g = enemyColors[2]
-			local b = math.random()*r
+			local g = teamRand1[teamID]*r
+			local b = teamRand2[teamID]*r
 			Spring.SetTeamColor(teamID, r,g,b)
 			
 			e = e+increase
 			if (e > 1) then  
-				e = math.random()
+				e = 0.1 + rand2
 			end			
 			
 		end
 	end
 	if not is_speccing then
-		Spring.SetTeamColor(myTeam, unpack(myColor))	-- overrides previously defined color
+		Spring.SetTeamColor(myTeam, unpack(myTrueColor))	-- overrides previously defined color
 	end
 end
 

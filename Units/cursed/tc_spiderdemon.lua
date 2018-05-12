96ca7f6a79322ae571e5101d9341357aae0793d9
azaremoth <azaremoth@users.noreply.github.com>
2018-05-13 00:42:07 +0200

    added spider demon. Animations, scripts, weapons, ... are still missing.

---
+++ Units/cursed/tc_spiderdemon.lua
@@ -0,0 +1,192 @@
+--------------------------------------------------------------------------------
+
+local unitName = "tc_spiderdemon"
+
+--------------------------------------------------------------------------------
+
+local unitDef = {
+  acceleration       = 0.75,
+  armortype          = "HEAVY",
+  badTargetCategory  = "AIR",
+  brakeRate          = 1.83105,
+  buildCostEnergy    = 940,
+  buildCostMetal     = 940,
+  buildPic           = "tc_spiderdemon.png",
+  buildTime          = 940,
+  canAttack          = true,
+  canGuard           = true,
+  canMove            = true,
+  canPatrol          = true,
+  canstop            = "1",
+  category           = "CURSED LAND HEAVYARMOR",
+  corpse             = "dead",
+  customParams       = {	
+	factionname	= "cursed",
+	normaltex = "unittextures/normalmaps/atlas_tc_mechanics_normal.png",
+	normalmaps = "yes",	
+    helptext	= "XXXX"	
+	},
+  defaultmissiontype = "Standby",
+  description        = "XXX",
+  --  energyMake         = "-3",
+  explodeAs          = "SMALL_EXPLOSION_GREEN",
+  firestandorders    = "1",
+  footprintX         = 2,
+  footprintZ         = 2,
+  iconType           = "bigcircle",
+  idleAutoHeal       = 2.5,
+  idleTime           = 400,
+  mass               = 1000,
+  maxDamage          = 1100,
+  maxSlope           = 14,
+  maxVelocity        = 2.8,
+  maxWaterDepth      = 24,
+  mobilestandorders  = "1",
+  movementClass      = "HEAVYWALKER2X2",
+  name               = "Spiderdemon",
+  nanoColor          = "0.6 1 0.15",
+  noAutoFire         = false,
+  noChaseCategory    = "AIR",
+  objectName         = "tc_spiderdemon.s3o",
+  script             = "tc_spiderdemon.lua",  
+  selfDestructAs     = "SMALL_EXPLOSION_GREEN",
+  showNanoFrame      = false,
+  side               = "cursed",
+  sightDistance      = 768,
+  smoothAnim         = false,
+  standingfireorder  = "2",
+  standingmoveorder  = "1",
+  
+  leavetracks            = 1,
+  trackOffset            = 0,
+  trackStrength          = 9,
+  trackStretch           = 1,
+  trackType              = [[Pointy]],
+  trackWidth             = 60,  
+  
+  turnRate           = 350,
+  unitname           = "tc_spiderdemon",
+  sfxtypes = {
+    explosiongenerators = {
+      "custom:MEDIUM_EXPLOSION_GREEN",
+      "custom:GunGroundFlash_Medium_long",
+    },
+  },
+  sounds = {
+    build              = "",
+    canceldestruct     = "",
+    repair             = "",
+    underattack        = "",
+    working            = "",
+    arrived = {
+      "",
+    },
+    cant = {
+      "",
+    },
+    count = {
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    },
+    ok = {
+      "agares_move",
+    },
+    select = {
+      "agares_select",
+    },
+  },
+  weapons = {
+    [1]  = {
+      def                = "AGARES_LASER",
+      mainDir            = "0 0 1",
+      maxAngleDif        = 160,
+      badTargetCategory = "AIR",	  
+      onlyTargetCategory = "LAND",
+    },
+  },
+}
+
+
+--------------------------------------------------------------------------------
+
+local weaponDefs = {
+  AGARES_LASER = {
+    avoidFriendly      = true,
+	collideFriendly    = true,  
+    accuracy           = 100,
+    areaOfEffect       = 32,
+    beamtime           = 0.75,
+    burnblow           = true,
+    coreThickness      = 0.4,
+    craterBoost        = 0,
+    craterMult         = 0,
+    energypershot           = 0,
+    explosionGenerator = "custom:AGARES_LASER",
+    fireStarter        = 20,
+    impulseBoost       = 0,
+    interceptedByShieldType = 1,
+    largeBeamLaser     = true,
+    laserFlareSize     = 3,
+    name               = "Agares Laser",
+	noSelfDamage     = true,
+    range              = 800,
+    reloadtime         = 7,
+    -- rendertype         = 0,
+	rgbColor           = {0.45, 1, 0.1},
+	rgbColor2 		   = {1, 1, 1},
+    soundHit           = "AgaresLaserHit",
+    soundStart         = "AgaresLaser",
+    soundTrigger       = true,
+	sweepFire		   = false,		
+    targetMoveError    = 0.33,
+    texture1           = "beam1",
+    texture2           = "null_texture",
+    texture3           = "null_texture",
+    texture4           = "flash1",
+    thickness          = 4,
+    tolerance          = 1500,
+    turret             = true,
+    weaponType         = "BeamLaser",
+    weaponVelocity     = 1500,
+    damage = {
+      default            = 1300,
+      flyer              = 1300*0.33,
+      heavy              = 1300,
+      light              = 1300,
+    },
+  },
+}
+unitDef.weaponDefs = weaponDefs
+
+
+--------------------------------------------------------------------------------
+
+local featureDefs = {
+  dead = {	
+	resurrectable	   = 0,  
+    blocking           = true,
+	customParams          = {
+		featuredecaytime		= "60"
+	}, 	
+    damage             = 8000,
+    description        = "Former Agares",
+    energy             = 0,
+    footprintX         = 2,
+    footprintZ         = 2,
+    height             = "25",
+    hitdensity         = "1000",
+    metal              = 0,
+    object             = "tc_spiderdemon_dead.s3o",
+    reclaimable        = true,
+  },
+}
+unitDef.featureDefs = featureDefs
+
+--------------------------------------------------------------------------------
+return lowerkeys({ [unitName] = unitDef })
+
+--------------------------------------------------------------------------------
---
+++ scripts/tc_spiderdemon.lua
@@ -0,0 +1,250 @@
+-- #include "basic_spring_stuff.h"
+
+local echo = Spring.Echo
+
+local base = piece 'base'
+local brain = piece 'brain'
+local tail1 = piece 'tail1'
+local tail2 = piece 'tail2'
+local tail3 = piece 'tail3'
+local tail4 = piece 'tail4'
+local rfleg1 = piece 'rfleg1'
+local rfleg2 = piece 'rfleg2'
+local rfleg3 = piece 'rfleg3'
+local rrleg1 = piece 'rrleg1'
+local rrleg2 = piece 'rrleg2'
+local rrleg3 = piece 'rrleg3'
+local lrleg1 = piece 'lrleg1'
+local lrleg2 = piece 'lrleg2'
+local lrleg3 = piece 'lrleg3'
+local lfleg1 = piece 'lfleg1'
+local lfleg2 = piece 'lfleg2'
+local lfleg3 = piece 'lfleg3'
+local emit1 = piece 'emit1'
+local emit2 = piece 'emit2'
+local summon = piece 'summon'
+
+local moving = false
+local attacking = false
+
+local activegun
+
+local MOVEANIMATIONSPEED
+local MOVEANIMATIONSLEEPTIME
+
+local	SIG_AIM1	=	2
+
+local	MOVELEGMAXANGLE = 40
+
+local SUMMONING			 = 1024+0
+local DefenderGunFlare	 = 1025+0
+local BLOODSPRAY	 = 1026+0
+
+local forward = 2.2
+local backward = 2
+local up = 1
+local sa = math.rad(-10)*50
+local ma = math.rad(40)*50
+
+local function Turn2(piecenum,axis, degrees, speed)
+	local radians = degrees * 3.1415 / 180
+	if speed then
+		local speed1 = speed * 3.1415 / 180
+		Turn(piecenum, axis, radians, speed1) 
+	else
+		Turn(piecenum, axis, radians ) 
+	end
+end
+
+local function SetMoveAnimationSpeed()
+	MOVEANIMATIONSPEED = (GetUnitValue(COB.MAX_SPEED)/2000)
+	MOVEANIMATIONSLEEPTIME = (55000000/GetUnitValue(COB.MAX_SPEED))
+	--if statements inside walkscript contain wait functions that can take forever if speed is too slow
+	if MOVEANIMATIONSPEED < 50 then 
+		MOVEANIMATIONSPEED = 50
+	end
+	if MOVEANIMATIONSLEEPTIME > 500 then 
+		MOVEANIMATIONSLEEPTIME = 500
+	end	
+end
+
+
+-- Walk Motion
+local function Walkscript()
+	while true do
+		SetMoveAnimationSpeed()	
+   		if moving then 
+
+		Turn2( lfleg1, y_axis, 1.5*ma, forward*MOVEANIMATIONSPEED )   	-- right front forward
+		Turn2( lfleg1, z_axis, -ma/2, up*MOVEANIMATIONSPEED )       	-- right front up
+		Turn2( lfleg2, z_axis, -ma/3, up*MOVEANIMATIONSPEED )
+				
+		Turn2( lrleg1, y_axis, -1.5*ma, backward*MOVEANIMATIONSPEED ) 	-- right back backward
+		Turn2( lrleg1, z_axis, 0, 6*up*MOVEANIMATIONSPEED )         	-- right back down
+		Turn2( lrleg2, z_axis, 0, up*MOVEANIMATIONSPEED )
+		
+		Turn2( rfleg1, y_axis, sa, backward*MOVEANIMATIONSPEED ) 	-- left front backward
+    	Turn2( rfleg1, z_axis, 0, 6*up*MOVEANIMATIONSPEED )         	-- left front down
+		Turn2( rfleg3, z_axis, 0, up*MOVEANIMATIONSPEED )
+		
+		Turn2( rrleg1, y_axis, -sa, forward*MOVEANIMATIONSPEED ) 	-- left back forward
+		Turn2( rrleg1, z_axis, ma/2, up*MOVEANIMATIONSPEED )       	-- left back up
+		Turn2( rrleg3, z_axis, ma/3, up*MOVEANIMATIONSPEED )
+	  
+		Sleep(MOVEANIMATIONSLEEPTIME)
+  
+   		end 
+   		if moving then 
+   		 
+		Turn2( lfleg1, y_axis, -sa, backward*MOVEANIMATIONSPEED ) 	-- right front backward
+		Turn2( lfleg1, z_axis, 0, 6*up*MOVEANIMATIONSPEED )         	-- right front down
+		Turn2( lfleg2, z_axis, 0, up*MOVEANIMATIONSPEED )
+		
+		Turn2( lrleg1, y_axis, sa, forward*MOVEANIMATIONSPEED )   	-- right back forward
+		Turn2( lrleg1, z_axis, -ma/2, up*MOVEANIMATIONSPEED )       	-- right back up
+		Turn2( lrleg2, z_axis, -ma/3, up*MOVEANIMATIONSPEED )
+		
+		Turn2( rfleg1, y_axis, -1.5*ma, forward*MOVEANIMATIONSPEED ) 	-- left front forward
+		Turn2( rfleg1, z_axis, ma/2, up*MOVEANIMATIONSPEED )       	-- left front up
+		Turn2( rfleg3, z_axis, ma/3, up*MOVEANIMATIONSPEED )
+		
+		Turn2( rrleg1, y_axis, 1.5*ma, backward*MOVEANIMATIONSPEED ) 	-- left back backward
+		Turn2( rrleg1, z_axis, 0, 6*up*MOVEANIMATIONSPEED )         	-- left back down
+		Turn2( rrleg3, z_axis, 0, up*MOVEANIMATIONSPEED )
+	 
+		Sleep(MOVEANIMATIONSLEEPTIME)
+
+   		end 
+
+		if not moving then
+		
+			Turn2( lrleg1, y_axis, -20, ((MOVEANIMATIONSPEED)*1.72)  )
+			Turn2( lfleg1, y_axis, 20, ((MOVEANIMATIONSPEED)*1.72)  )
+			Turn2( lrleg1, z_axis, 0, ((MOVEANIMATIONSPEED)*1.72)  )
+			Turn2( lfleg1, z_axis, 0, ((MOVEANIMATIONSPEED)*1.72)  )
+
+			Turn2( lrleg2, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( lrleg3, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( lfleg2, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( lfleg3, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+					
+			Turn2( rfleg1, y_axis, -20, ((MOVEANIMATIONSPEED)*1.72)  )
+			Turn2( rrleg1, y_axis, 20, ((MOVEANIMATIONSPEED)*1.72)  )
+			Turn2( rfleg1, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( rrleg1, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+	  
+			Turn2( rfleg2, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( rfleg3, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( rrleg2, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )
+			Turn2( rrleg3, z_axis, 0, ((MOVEANIMATIONSPEED)*1.17)  )	
+			
+			Sleep(MOVEANIMATIONSLEEPTIME)
+        end
+		Sleep(50)
+	end
+end
+
+
+function script.Create()
+
+   	Turn2( summon, x_axis, -90 )
+   	Turn2( lrleg1, y_axis, -20 ) 
+   	Turn2( lfleg1, y_axis, 20 ) 
+    Turn2( rfleg1, y_axis, -20 ) 
+	Turn2( rrleg1, y_axis, 20 ) 
+
+	moving = false
+	attacking = false	
+	activegun = 1
+	
+
+		
+	----------------------------------START BUILD CYCLE
+	while  GetUnitValue( COB.BUILD_PERCENT_LEFT ) > 0 do
+			EmitSfx(summon, SUMMONING)
+			Sleep(300)
+	end
+	--------------------------------/END BUILD CYCLE
+	StartThread( Walkscript )
+end
+
+function script.StartMoving()
+ moving=true 
+ end
+
+function script.StopMoving()
+ moving=false 
+end
+
+--weapon 1 -----------------------------------------------------------------
+
+local function RestoreAfterDelay()
+	Sleep(5000)
+	attacking = false	
+    activegun = 0
+end
+
+function script.AimWeapon1(heading, pitch)
+	Signal(SIG_AIM1)
+	SetSignalMask(SIG_AIM1)
+	attacking = true	
+    StartThread( RestoreAfterDelay) 
+    return (1)
+end
+
+function script.FireWeapon1()
+
+	if  activegun ~= 1  then
+	
+		EmitSfx(emit1, DefenderGunFlare)
+		Sleep(100)
+		activegun = 1
+		return(1)
+	end
+	if  activegun == 1  then
+	
+		EmitSfx(emit2, DefenderGunFlare)
+		Sleep(100)
+		activegun = 0
+		return(1)
+	end
+end
+
+--AimFromWeapon
+
+function script.AimFromWeapon1(piecenum)
+
+	return gun
+end
+
+function script.QueryWeapon1(piecenum)
+
+	if  activegun ~= 1  then
+	
+		return emit1
+	end
+	if  activegun == 1  then
+	
+		return emit2
+	end
+end
+
+	
+function script.Killed(recentDamage,maxHealth )
+
+	local severity = recentDamage/maxHealth
+
+	Explode(rfleg1, math.bit_or(SFX.FALL, SFX.EXPLODE_ON_HIT ) )
+	Explode(rfleg2, math.bit_or(SFX.FALL, SFX.EXPLODE_ON_HIT ) )	
+	Explode(tail1, math.bit_or(SFX.FALL , SFX.SMOKE , SFX.EXPLODE_ON_HIT ) )
+	Explode(tail2, math.bit_or(SFX.FALL , SFX.SMOKE , SFX.EXPLODE_ON_HIT ) )
+	EmitSfx(brain, BLOODSPRAY)
+	
+	if severity <= 0.5 then
+		corpsetype = 1
+		return (corpsetype)
+	end	
+	corpsetype = 2
+	return (corpsetype)
+	
+end

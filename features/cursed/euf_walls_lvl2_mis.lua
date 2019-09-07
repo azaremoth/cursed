04817ac3e1621fcfb08d0178971f3b1dce68ea31
azaremoth <azaremoth@users.noreply.github.com>
2019-09-08 00:32:54 +0200

    features features...

---
+++ features/cursed/euf_walls_lvl2_mis.lua
@@ -0,0 +1,29 @@
+local objectname= "euf_walls_lvl2_mis" 
+local featureDef  =  {
+   name           = "euf_walls_lvl2_mis",  
+	resurrectable	   = 0,  
+    category         = [[dragonteeth]],
+    nodrawundergray  = false,  
+    autoreclaimable    = "0",
+    blocking           = true,
+	mass			   = 1000000000,
+	customparams = { 
+		normaltex 		   	= "unittextures/normalmaps/atlas_euf_buildings_normal.png",
+		normalmaps 		   	= "yes",
+	}, 	
+  collisionVolumeType 		= "Box",
+  collisionVolumeScales 	= "80 70 10",
+  collisionVolumeOffsets 	= "0 0 5",	
+    damage             = 1000000000,
+    description        = "Defensewall",
+    footprintX         = 5,
+    footprintZ         = 1,
+    height             = "15",
+    hitdensity         = "1000000000",
+    metal              = 20,
+    object             = "euf_wall_lvl2.s3o",
+    reclaimable        = false,
+    upright            = true,
+    world              = "allworld", 
+}
+return lowerkeys({[featureDef.name] = featureDef})

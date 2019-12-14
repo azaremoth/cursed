6b95a91207f6711bdc73747504691a7b67a6b5b5
azaremoth <azaremoth@users.noreply.github.com>
2019-12-14 23:07:07 +0100

    further stuff shifted to tcampaign

--- LuaUI/Configs/ignore.lua
+++ LuaUI/Configs/ignore.lua
@@ -1,27 +1,13 @@
 -- ignore List for e.g. team platter widget
 local IgnoreUnit = {
-	[UnitDefNames.euf_transport_mis.id] = "euf_transport_mis",
-	[UnitDefNames.grounddecal.id] = "grounddecal",
+	[UnitDefNames.burrowcrust.id] = "burrowcrust",
+	[UnitDefNames.terraunit.id] = "terraunit",
+	[UnitDefNames.fakeunit_aatarget.id] = "fakeunit_aatarget",
+	[UnitDefNames.grounddecal.id] = "grounddecal",	
 	[UnitDefNames.trails1.id] = "trails1",
 	[UnitDefNames.trails2.id] = "trails2",
 	[UnitDefNames.trails3.id] = "trails3",
 	[UnitDefNames.trails4.id] = "trails4",
-	[UnitDefNames.burrowcrust.id] = "burrowcrust",
-	[UnitDefNames.converter.id] = "converter",
-	[UnitDefNames.noemit_high.id] = "noemit_high",
-	[UnitDefNames.terraunit.id] = "terraunit",
-	[UnitDefNames.fakeunit_aatarget.id] = "fakeunit_aatarget",
-	[UnitDefNames.emit_fire.id] = "emit_fire",
-	[UnitDefNames.emit_fire_mid.id] = "emit_fire_mid",
-	[UnitDefNames.emit_fire_high.id] = "emit_fire_high",
-	[UnitDefNames.emit_smoke.id] = "emit_smoke",
-	[UnitDefNames.emit_smoke_mid.id] = "emit_smoke_mid",
-	[UnitDefNames.emit_smoke_high.id] = "emit_smoke_high",
-	[UnitDefNames.emit_medsmoke.id] = "emit_medsmoke",
-	[UnitDefNames.emit_medsmoke_mid.id] = "emit_medsmoke_mid",
-	[UnitDefNames.emit_medsmoke_high.id] = "emit_medsmoke_high",
-	[UnitDefNames.spawner.id] = "spawner",
-	[UnitDefNames.spawner_ghoul.id] = "spawner_ghoul",
 	}
 
 return IgnoreUnit
